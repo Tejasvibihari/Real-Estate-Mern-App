815e220939f8ed35d03bc15212496efc83fb64b1
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-16 23:45:05 +0530

    api folder create

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container">
+                    <a className="navbar-brand" href="/"><span className="text-success">Tejasvi</span><span className="text-danger">Realestate</span></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <form className="d-flex mx-auto" role="search"> {/* Added mx-auto to center the form */}
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> {/* Changed mx-auto to ms-auto to push items to the right */}
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/sign-in">Sign In</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+    )
+}
