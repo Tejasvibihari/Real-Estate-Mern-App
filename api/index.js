2a5764126914d5eb3859fa1a88a445a80abb34db
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-17 13:48:12 +0530

    Connect to database and create user model

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 
 const app = express();
+dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => { console.log("Connected to Database NotesDb"); })
+    .catch((err) => { console.log(err); });
 
 
 app.listen(3000, () => {
