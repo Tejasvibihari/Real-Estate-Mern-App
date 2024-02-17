0793306c519f9ab886e77b744a6929fcb0aca1c5
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-17 15:20:55 +0530

    Create auth route and saved the data in the database with password hassing

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt';
+import User from '../models/user.model.js';
+const saltRounds = 10;
+export const auth = async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const hash = await bcrypt.hash(password, saltRounds);
+        const newUser = new User({
+            username: username,
+            email: email,
+            password: hash
+        })
+        await newUser.save()
+            .then(() => {
+                console.log("Saved");
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        console.log("User saved successfully" + newUser);
+
+
+    } catch (error) {
+        console.log(error);
+        res.send("Internal server Error" + error);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './router/user.route.js';
+import authRouter from './router/auth.route.js';
 
 
 const app = express();
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => { console.log("Connected to Database NotesDb"); })
@@ -14,3 +17,6 @@ mongoose.connect(process.env.MONGODB_URL)
 app.listen(3000, () => {
     console.log("Server is running on port 3000...");
 })
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
