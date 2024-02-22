e3ccc252c3b2acaa534c54e64a60753115054f19
Tejasvi Bihari <tejasvibihari2000@gmail.com>
2024-02-22 11:04:32 +0530

    Made sigin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 const saltRounds = 10;
+
+// SignUp Controller 
 export const signup = async (req, res, next) => {
     const username = req.body.username;
     const email = req.body.email;
@@ -15,6 +19,36 @@ export const signup = async (req, res, next) => {
         })
         await newUser.save()
         res.json('Signup successful');
+    } catch (error) {
+        next(error)
+    }
+}
+
+// SignIn Controller
+
+export const signin = async (req, res, next) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+        const user = await User.findOne({ email: email });
+        if (user === null) {
+            // console.log('User not found');
+            return next(errorHandler(404, 'User not found'));
+        } else {
+            const hashValidPassword = await bcrypt.compare(password, user.password);
+            if (!hashValidPassword) {
+                //console.log('Invalid Password');
+                return next(errorHandler(404, 'Invalid Password'));
+            }
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.cookie('access_token', token, { httpOnly: true, maxAge: 60 * 60 * 24 * 7 })
+                .status(200)
+                .json(hashValidPassword);
+            // res.json({ token });
+            // console.log(token);
+
+        }
     } catch (error) {
         next(error)
     }
